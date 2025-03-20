3e34ba9f81e8471ff1edf2c9891926ac9a0ee360
DevangAjudiya <devangajudiya@gmail.com>
2025-03-20 15:06:50 +0530

    Update PG-5.py

--- PG-5.py
+++ PG-5.py
@@ -1,14 +1,16 @@
 def ispangram(st):
     st=set(st)
-    st.remove(" ")
+    st2=set()
+    for i in range(97,123):
+        st2.update(chr(i))
+    st= st & st2
     for i in range(65,91):
         if chr(i) in st:
-            pass
-        elif chr(i+32) in st:
             pass
         else:
             return False
     return True
 a=input("enter the string=")
+a=a.lower()
 b= ispangram(a)
 print(b)
